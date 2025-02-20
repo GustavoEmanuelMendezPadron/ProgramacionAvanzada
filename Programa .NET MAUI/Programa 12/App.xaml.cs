3035ffac6ea54ad6afce6e01cf62ca3b30e8027a
GustavoEmanuelMendezPadron <twyxzmusic@gmail.com>
2025-02-20 13:50:34 -0600

    Add files via upload

---
+++ Programa .NET MAUI/Programa 12/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace CheckConnectivityMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Programa .NET MAUI/Programa 12/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace CheckConnectivityMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        Label ConnectionStatusLabel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ConnectionStatusLabel = new Label
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20
+            };
+
+            Content = new StackLayout
+            {
+                Children = { ConnectionStatusLabel }
+            };
+
+            CheckNetworkStatus();
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+        }
+
+        private void CheckNetworkStatus()
+        {
+            var current = Connectivity.NetworkAccess;
+            ConnectionStatusLabel.Text = current == NetworkAccess.Internet ? "Conectado a Internet" : "Sin conexión";
+        }
+
+        private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            CheckNetworkStatus();
+        }
+    }
+}
