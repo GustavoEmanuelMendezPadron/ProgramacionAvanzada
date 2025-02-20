e3ee4173a0ed52e09f530e705dbb697467090461
GustavoEmanuelMendezPadron <twyxzmusic@gmail.com>
2025-02-20 13:30:57 -0600

    Add files via upload

---
+++ Programa .NET MAUI/Programa 3/a2233336160_Tareas_Unidad01p2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace a2233336160_Tareas_Unidad01p2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Programa .NET MAUI/Programa 3/a2233336160_Tareas_Unidad01p2/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Windows;
+
+namespace amatricula_tareas_unidad01
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Texto ingresado: " + InputTextBox.Text);
+        }
+    }
+}
