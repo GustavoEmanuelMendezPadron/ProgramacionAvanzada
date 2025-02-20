fc61aad79200c9b9e16445f1cb15d7a2e2ca1395
GustavoEmanuelMendezPadron <twyxzmusic@gmail.com>
2025-02-20 13:35:05 -0600

    Add files via upload

---
+++ Programa .NET MAUI/Programa 4/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        CreateMenuBar();
+    }
+
+    private void CreateMenuBar()
+    {
+        ToolbarItems.Add(new ToolbarItem("Exit", null, () =>
+        {
+            Application.Current.Quit();
+        }));
+    }
+
+    private void OnShowFormClicked(object sender, EventArgs e)
+    {
+        formFrame.IsVisible = !formFrame.IsVisible;
+        btnShowForm.Text = formFrame.IsVisible ? "Hide Form" : "Show Form";
+    }
+
+    private void OnSubmitClicked(object sender, EventArgs e)
+    {
+        var gender = maleRadio.IsChecked ? "Male" :
+                    femaleRadio.IsChecked ? "Female" : "Not specified";
+
+        var data = $"Name: {nameEntry.Text}\n" +
+                   $"Occupation: {occupationEntry.Text}\n" +
+                   $"Age: {agePicker.SelectedItem}\n" +
+                   $"Employment: {employmentPicker.SelectedItem}\n" +
+                   $"US Citizen: {(usCitizenCheckBox.IsChecked ? "Yes" : "No")}\n" +
+                   $"Gender: {gender}";
+
+        displayEditor.Text = data;
+    }
+}
