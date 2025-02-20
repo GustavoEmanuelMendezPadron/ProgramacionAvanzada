775631dce734d66a0f1e1be83b34e2e026d8711f
GustavoEmanuelMendezPadron <twyxzmusic@gmail.com>
2025-02-20 13:38:42 -0600

    Add files via upload

---
+++ Programa .NET MAUI/Programa 5/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Phoneword;
+
+public partial class MainPage : ContentPage
+{
+    string translatedNumber;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = $"Llamar a {translatedNumber}";
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Llamar";
+        }
+    }
+
+    private async void OnCall(object sender, EventArgs e)
+    {
+        if (await this.DisplayAlert(
+            "Llamar",
+            $"¿Quiere llamar al número {translatedNumber}?",
+            "Sí",
+            "No"))
+        {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported)
+                    PhoneDialer.Default.Open(translatedNumber);
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Error", "No se puede realizar la llamada.", "OK");
+            }
+        }
+    }
+}
